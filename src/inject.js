1ee2a16cdd381ee8395518a4f55d01c607bb788b
Stanislav <Stanislav@fksis.by>
2016-10-22 13:31:56 +0300

    add getExchangeRatesFromStorage

--- src/inject.js
+++ src/inject.js
@@ -5,7 +5,7 @@
 $(function () {
     console.log('It works!');
     //debugger;
-    getExchangeRates();
+    getExchangeRatesFromStorage();
     $('#div_shoppingCart').on('change', function () {
         console.log('select works');
     })
@@ -29,45 +29,22 @@ function calculateCustomsDuty(euroExchangeRate, usdExchangeRate, orderPrice) {
 
 }
 
-function getExchangeRates() {
-    var today = new Date();
-    var dateString = today.toISOString().substring(0, 10);
-    debugger;
-    $.ajax({
-        url: `https://www.nbrb.by/API/ExRates/Rates?onDate=${dateString}&Periodicity=0`,
-        type: "GET",
-        success: setExchangeRates,
-        error: setDefaultExchangeRates,
-    });
+function getExchangeRatesFromStorage() {
+    chrome.storage.local.get(setExchangeRates);
 }
 
-
-function setExchangeRates(data) {
+function setExchangeRates(items) {
     debugger;
-    for (let element of data) {
-        var a = 3;
-        switch (element['Cur_ID']) {
-            case 292:
-                var euro = element['Cur_OfficialRate'];
-                break;
-            case 145:
-                var usd = element['Cur_OfficialRate'];
-                break;
-        }
-    }
-    debugger;
-    handleData(euro, usd);
+    var euroExchangeRate = items.euroExchangeRate;
+    var usdExchangeRate = items.usdExchangeRate;
+    handleData(euroExchangeRate, usdExchangeRate);
 }
 
-
-function setDefaultExchangeRates() {
-    console.error('Курс из нац банка не подгружен!');
-    handleData(2.2, 2);
-}
 
 function handleData(euroExchangeRate, usdExchangeRate) {
     debugger;
     var customsDuty = calculateCustomsDuty(euroExchangeRate, usdExchangeRate, getOrderPrice());
+    getTotalsPrice();
     $('#shopping-cart').find('.shopping-cart-row .summary-section .summary-actions')
         .before(createHtmlForCustomsDuty(customsDuty));
     //.before('<p>tst</p>');
@@ -76,7 +53,7 @@ function handleData(euroExchangeRate, usdExchangeRate) {
 function createHtmlForCustomsDuty(customsDuty) {
     var text =
         `<section class="col-xs-24 float-right last-col">
-            <p class="totals">
+            <p class="totals extension-lines">
                 <span class="type">Пошлина РБ:</span>
                 <span class="price">$${customsDuty}</span>
             </p>
@@ -89,3 +66,30 @@ function getOrderPrice() {
     return parseFloat(text.slice(1))
 }
 
+function getTotalsPrice() {
+    var text = $('#pSubtotal').text();
+    return parseFloat(text.slice(1))
+}
+
+// function setExchangeRates(data) {
+//     debugger;
+//     for (let element of data) {
+//         var a = 3;
+//         switch (element['Cur_ID']) {
+//             case 292:
+//                 var euro = element['Cur_OfficialRate'];
+//                 break;
+//             case 145:
+//                 var usd = element['Cur_OfficialRate'];
+//                 break;
+//         }
+//     }
+//     debugger;
+//     handleData(euro, usd);
+// }
+//
+//
+// function setDefaultExchangeRates() {
+//     console.error('Курс из нац банка не подгружен!');
+//     handleData(2.2, 2);
+// }
