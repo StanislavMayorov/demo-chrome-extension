0d4d392a9f1c3fa351314242ba3f6812d07a908b
Stanislav <Stanislav@fksis.by>
2016-10-22 16:11:24 +0300

    add remove, save listener

--- src/inject.js
+++ src/inject.js
@@ -3,13 +3,29 @@
 //292 euro
 
 $(function () {
-    console.log('It works!');
-    //debugger;
     getExchangeRatesFromStorage();
-    $('#div_shoppingCart').on('change', function () {
-        console.log('select works');
-    })
+    attachRemoveListener();
+    attachQuantityListener();
 });
+
+function attachRemoveListener() {
+    $('.save-or-remove').click(function () {
+        setTimeout(function () {
+            debugger;
+            getExchangeRatesFromStorage();
+        }, 3500);
+    });
+}
+
+function attachQuantityListener() {
+    $('#div_shoppingCart').on('change', function () {
+        setTimeout(function () {
+            debugger;
+            getExchangeRatesFromStorage();
+            attachRemoveListener();
+        }, 700);
+    });
+}
 
 function calculateCustomsDuty(euroExchangeRate, usdExchangeRate, orderPrice) {
     //(45 - 22* 1.1) * 0.3 + оформление + почтовый сбор.
@@ -22,9 +38,9 @@ function calculateCustomsDuty(euroExchangeRate, usdExchangeRate, orderPrice) {
         return 0
     }
     else {
-        return Number((orderPrice - customsFreeUsd) * 0.3 +
+        return (orderPrice - customsFreeUsd) * 0.3 +
             customsRegistrationFeeEuro * euroToUsd +
-            postServiceRub / usdExchangeRate).toFixed(2);
+            postServiceRub / usdExchangeRate;
     }
 
 }
@@ -34,7 +50,6 @@ function getExchangeRatesFromStorage() {
 }
 
 function setExchangeRates(items) {
-    debugger;
     var euroExchangeRate = items.euroExchangeRate;
     var usdExchangeRate = items.usdExchangeRate;
     handleData(euroExchangeRate, usdExchangeRate);
@@ -42,20 +57,24 @@ function setExchangeRates(items) {
 
 
 function handleData(euroExchangeRate, usdExchangeRate) {
-    debugger;
     var customsDuty = calculateCustomsDuty(euroExchangeRate, usdExchangeRate, getOrderPrice());
-    getTotalsPrice();
+    var sum = customsDuty + getTotalsPrice();
     $('#shopping-cart').find('.shopping-cart-row .summary-section .summary-actions')
-        .before(createHtmlForCustomsDuty(customsDuty));
-    //.before('<p>tst</p>');
+        .before(createHtmlForCustomsDuty(customsDuty.toFixed(2), sum.toFixed(2)));
 }
 
-function createHtmlForCustomsDuty(customsDuty) {
+function createHtmlForCustomsDuty(customsDuty, sum) {
     var text =
         `<section class="col-xs-24 float-right last-col">
             <p class="totals extension-lines">
                 <span class="type">Пошлина РБ:</span>
                 <span class="price">$${customsDuty}</span>
+            </p>
+        </section>
+        <section class="col-xs-24 float-right last-col">
+            <p class="totals grand extension-lines">
+                <span class="type">Итог:</span>
+                <span class="price">$${sum}</span>
             </p>
         </section>`;
     return $(text);
@@ -67,7 +86,7 @@ function getOrderPrice() {
 }
 
 function getTotalsPrice() {
-    var text = $('#pSubtotal').text();
+    var text = $('#shopping-cart').find('.shopping-cart-row .summary-section .totals.grand .price').text();
     return parseFloat(text.slice(1))
 }
 
